dde7491a0c9e10fd04be3d31b85350518c14a8e7
Seif Elnawawy <elnawawyseif@gmail.com>
2025-03-03 03:52:26 +0200

    set the core folder

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blueColor = Color(0xFF0D47A1);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color lightBlackColor = Color(0xFF333333);
+  static const Color hintsColor = Color(0xFF808080);
+  static const Color greenColor = Color(0xFF00C853);
+  static const Color redColor = Color(0xFFD32F2F);
+}
